d404f05d2967e9cdfdabbb0c476aeddc720ec52b
manumanojm <manurao24899@gmail.com>
2025-04-28 13:11:05 +0530

    contents changed

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-window.onload = function() {
-    alert('Welcome to my portfolio!');
-};
+document.getElementById("revealMore").addEventListener("click", function() {
+    var moreInfo = document.getElementById("moreInfo");
+    moreInfo.classList.toggle("hidden");
+
+    // Change button text based on visibility
+    if (moreInfo.classList.contains("hidden")) {
+        this.innerText = "Learn More";
+    } else {
+        this.innerText = "Show Less";
+    }
+});
